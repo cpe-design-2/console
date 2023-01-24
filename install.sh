ef9a2b82c012dda1925258957dca832dcc1482a3
Chase Ruskin <c.ruskin@ufl.edu>
2023-01-24 15:49:16 -0500

    begins overall installation script

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# File: install.sh
+# Author: Chase Ruskin
+# Details:
+#   This file contains the entire installation required for setting up the open-
+#   source video game console 'goco'.
+#
+#   This installation assumes the user is using a newly configured Pi with the
+#   provided specifications.
+#
+#   Hardware: Raspberry Pi 3 Model B V1.2
+#   Storage: 16GB microSD card
+#   Operating System: Raspberry Pi OS (64-bit) [Released: 2022-09-22]
+#
+#   Download this script and execute it:
+#
+#   $ sh ./install.sh
+#
+
+# stop installation on any intermediate error
+set -e
+
+# 1) COMPILE GODOT GAME ENGINE
+
+GODOT_VERSION="3.5.1-stable"
+
+FRT_VERSION="2.0"
+
+# install dependencis for godot
+sudo apt-get install git build-essential scons pkg-config clang llvm lld libsdl2-dev libgles2-mesa-dev
+
+# clone the godot repository at the specified version (tag)
+git clone -b $GODOT_VERSION https://github.com/godotengine/godot.git ./godot
+
+# add the FRT paltform for the RPi3
+cd ./godot/platform
+git clone -b $FRT_VERSION https://github.com/efornara/frt
+# return to godot's base folder
+cd ..
+
+# execute the build process
+scons platform=frt tools=no target=release use_llvm=yes -j 4 module_webm_enabled=no
+
+# place the compiled executable in a known path
+
+
+
+# 2) UPDATE .BASHRC
