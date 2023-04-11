3ed54394316ceabf0c58ed4aadcc863b1e7337a7
Chase Ruskin <c.ruskin@ufl.edu>
2023-04-11 18:26:10 -0400

    allows godot instance checking for modern computer targets

--- src/engine.rs
+++ src/engine.rs
@@ -32,6 +32,13 @@ impl Engine {
     /// This function internally checks if a game is already running and will only
     /// invoke a game if no child process is found.
     pub fn play_game(&mut self, game: &Game) -> () {
+        #[cfg(not(feature = "rpi"))]
+        {
+            // check if the PID still exists (external event may have quit GODOT)
+            if self.is_in_game() == true && self.check_proc_id_alive() == false {
+                self.child = None;
+            } 
+        }
         if self.is_in_game() == false {
             // check if the executable exists
             match Path::new(&self.exe).is_file() {
@@ -65,6 +72,50 @@ impl Engine {
     /// process.
     pub fn is_in_game(&self) -> bool {
         self.child.is_some()
+    }
+
+    /// Runs a subprocess to check the currently running processes if the Godot
+    /// application is still actively running.
+    /// 
+    /// This function is only required for targets not built for the RaspberryPi.
+    #[cfg(not(feature = "rpi"))]
+    fn check_proc_id_alive(&mut self) -> bool {
+        // allow multiple instances of Godot on windows because of unavailable 'ps' command
+        if cfg!(target_os = "windows") == true {
+            return false;
+        }
+
+        if let Some(id) = self.child {
+            match std::process::Command::new("ps")
+                .arg("-p")
+                .arg(id.to_string())
+                .output()
+            {
+                // store the child's ID for future usage
+                Ok(rc) => {
+                    if rc.status.success() == true {
+                        // read the output string
+                        let text = String::from_utf8(rc.stdout).unwrap();
+                        // the process is being idle (essentially "killed")
+                        if text.contains("0:00.00") == true {
+                            false
+                        // the process is actively running
+                        } else {
+                            true
+                        }
+                    // the PID is not available to be found (essentially "killed")
+                    } else {
+                        false
+                    }
+                }
+                Err(e) => {
+                    eprintln!("error: {}", e);
+                    false
+                },
+            }
+        } else {
+            false
+        }
     }
 
     /// Kills the currently owned game process and clears the child id.
